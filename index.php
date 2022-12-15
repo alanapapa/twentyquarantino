268499da8d0cc1bd41a6f573cc8524b14b200fc8
hoot hoot <alanapapa@Macintosh.local>
2022-12-15 17:03:31 +0500

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main template file
+ * 
+ * @package Twenty Quarantino
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WP theme</title>
+</head>
+<body>
+  Hello WP theme
+</body>
+</html>
